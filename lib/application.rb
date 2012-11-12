6f0c4b23a5b5b83a8ea3b8000f70168e261d2ff9
Yukimitsu Izawa <izawa@izawa.org>
2012-11-12 22:34:07 +0900

    add tick label

--- lib/application.rb
+++ lib/application.rb
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 require 'rubygems' unless deployed?
 require 'hotcocoa'
-require 'pp'
 
 if deployed?
   ICON = '/Applications' + File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'Resources', 'Himawari.png'))
@@ -20,37 +19,36 @@ class RandWallpaper
     @status = status_item
     set_status_menu()
     load_prefs()
+
     @app.run()
   end
 
   def set_status_menu
     @menu = status_menu()
     @status.menu = @menu
-    # @status.title = "Hello" # アイコンの横にタイトルを付ける
-    @status.image = image(:file => ICON, :size => [ 20, 20 ])           if(File.exists?(ICON))
-    @status.alternateImage = image(:file => ICON2, :size => [ 20, 20 ]) if(File.exists?(ICON2))
-    #@status.setHighlightMode(true)
+    @status.image = image(file: ICON, size: [ 20, 20 ]) if(File.exists?(ICON))
+    @status.alternateImage = image(file: ICON2, size: [ 20, 20 ]) if(File.exists?(ICON2))
   end
 
   def status_menu
-    menu(:delegate => self) do |status|
-      status.item("Change", :on_action => Proc.new{ change })
+    menu(delegate: self) do |status|
+      status.item("Change", on_action: Proc.new{ change })
       status.separator()
       status.submenu(:menu) do |apple|
-        apple.item(:about, :title => "About #{NSApp.name}")
+        apple.item(:about, title: "About #{NSApp.name}")
         apple.separator()
-        apple.item(:preferences, :key => ",", :on_action => Proc.new{ config } )
+        apple.item(:preferences, key: ",", on_action: Proc.new{ config } )
         apple.separator()
         apple.submenu(:services)
         apple.separator()
-        apple.item(:hide, :title => "Hide #{NSApp.name}", :key => "h")
-        apple.item(:hide_others, :title => "Hide Others", :key => "h", :modifiers => [:command, :alt])
-        apple.item(:show_all, :title => "Show All")
+        apple.item(:hide, title: "Hide #{NSApp.name}", key: "h")
+        apple.item(:hide_others, title: "Hide Others", key: "h", modifiers: [:command, :alt])
+        apple.item(:show_all, title: "Show All")
         apple.separator()
-        apple.item(:quit, :title => "Quit #{NSApp.name}", :key => "q")
+        apple.item(:quit, title: "Quit #{NSApp.name}", key: "q")
       end
       status.separator()
-      status.item("Quit", :key => "q", :on_action => Proc.new { @app.terminate(self) })
+      status.item("Quit", key: "q", on_action: Proc.new { @app.terminate(self) })
     end
   end
 
@@ -63,44 +61,55 @@ class RandWallpaper
         @img_path.setEditable(nil)
         v << button(title: '.', on_action: Proc.new{ fsel }, frame:[315, 120, 29,30])
         v << label(text: '自動切り替え時間', layout: {start: false}, frame: [26,73, 228, 17])
-        v << @slider = slider(frame: [27, 47, 286, 21])
-        @slider.setMaxValue(3600)
-        @slider.setMinValue(0)
-        @slider.setNumberOfTickMarks(13)
+        v << @slider = slider(max: 3600, min: 0, tic_marks: 13, frame: [27, 47, 315, 21], on_action: Proc.new {|sec| set_timeout(sec) })
         @slider.setAllowsTickMarkValuesOnly(true)
+        @slider.setFloatValue(@interval)
+
+        v << label(text: 'なし', frame:[23, 25, 46, 17], font: font(name: "Tahoma", size: 9))
+        5.step(60, 5) { |min|
+          v << label(text: "#{min}分", frame:[25+min*5, 25, 46, 17], font: font(name: "Tahoma", size: 9))
+        }
       end
     end
   end
 
+  def set_timeout(sec)
+    
+    user_defaults.setObject(sec.to_i, forKey: 'interval')
+    user_defaults.synchronize
+  end
+
   def fsel
-    a=NSOpenPanel.openPanel
-    a.setCanChooseFiles(nil)
-    a.setCanChooseDirectories(true)
-    # if a.runModalForDirectory(nil, file:nil) == NSOKButton
-    if a.runModal == NSOKButton
-      @img_path.text = a.URLs[0].path
-      @img_dir = a.URLs[0].path
-      user_defaults.setObject(a.URLs[0].path, forKey:'img_dir')
+    panel = NSOpenPanel.openPanel
+    panel.setCanChooseFiles(nil)
+    panel.setCanChooseDirectories(true)
+    if panel.runModal == NSOKButton
+      @img_path.text = panel.URLs[0].path
+      @img_dir = panel.URLs[0].path
+      user_defaults.setObject(panel.URLs[0].path, forKey: 'img_dir')
       user_defaults.synchronize
     end
   end
 
   def load_prefs
     unless user_defaults.objectForKey('img_dir')
-      user_defaults.setObject(File.expand_path("~/Pictures"), forKey:'img_dir' )
-      p user_defaults.synchronize
+      user_defaults.setObject(File.expand_path("~/Pictures"), forKey: 'img_dir')
+      user_defaults.synchronize
     end
     @img_dir = user_defaults.objectForKey('img_dir')
+
+    unless user_defaults.objectForKey('interval')
+      user_defaults.setObject(0, forKey: 'interval')
+      user_defaults.synchronize
+    end
+    @interval = user_defaults.objectForKey('interval')
+
   end
 
-
   def change
-    #wallpaper_dir = File.expand_path("~/myphotos")
     wallpaper_dir = @img_dir
-   
-    p wallpaper_dir
-
     workspace = NSWorkspace.sharedWorkspace
+
     NSScreen.screens.each do |screen|
       wallpaper_path = 
         Dir.glob("#{wallpaper_dir}/**/*.{jpg,JPG,jpeg,JPEG}").sample
@@ -114,32 +123,6 @@ class RandWallpaper
       end
     end
   end
-
-
-  # file/open
-  def on_open(menu)
-  end
-
-  # file/new
-  def on_new(menu)
-  end
-
-  # help menu item
-  def on_help(menu)
-  end
-
-  # This is commented out, so the minimize menu item is disabled
-  #def on_minimize(menu)
-  #end
-
-  # window/zoom
-  def on_zoom(menu)
-  end
-
-  # window/bring_all_to_front
-  def on_bring_all_to_front(menu)
-  end
-
 end
 
 
